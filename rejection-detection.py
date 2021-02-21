a961b56f125a3876ba06ed7edfed3b3465085d88
sethpoly <emailspolyniak@gmail.com>
2021-02-21 12:26:02 -0500

    First commit

---
+++ rejection-detection.py
@@ -0,0 +1,50 @@
+import imaplib
+import email
+from email.header import decode_header
+import webbrowser
+import os
+
+# account credentials
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+
+# create IMAP4 class with SSL
+imap = imaplib.IMAP4_SSL('imap.gmail.com')
+# authenticate (if fails: <allow less secure apps in gmail account>)
+try:
+    (retcode, capabilities) = imap.login(username, password)
+    print(f'Logged in as {username}.' )
+except imaplib.IMAP4.error:
+    print('Log in failed.')
+
+# Connect to mailbox
+imap.select('INBOX')
+
+n = 0
+(retcode, messages) = imap.search(None, '(UNSEEN)') # Filter by unseen messages
+if retcode == 'OK':
+    for num in messages[0].split():
+        print('Processing: ')
+        n = n + 1
+        typ, data = imap.fetch(num, '(RFC822)')
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                original = email.message_from_bytes(response_part[1])
+
+                print(original['From'])
+                print(original['Subject'])
+
+                # Grab the body of the email
+                if original.is_multipart():
+                    for payload in original.get_payload():
+                        print(payload.get_payload())
+                else:
+                    print(original.get_payload())
+
+                typ, data = imap.store(num,'+FLAGS','\\Seen') # Mark msg as seen
+try:
+    imap.close()
+    print('Closed the connection.')
+except imaplib.IMAP4.error:
+    print('Error: Connection failed to close.')
+
