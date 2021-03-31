75b6667e1622427ba4ca7f13bb515527c8e9d209
sethpoly <emailspolyniak@gmail.com>
2021-03-31 15:33:59 -0400

    Helper func to help organize spreadsheet data to built data dashboard

--- service_account.py
+++ service_account.py
@@ -1,5 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import re
+
 
 class Spreadsheet:
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']  # define the scope
@@ -20,3 +22,19 @@ class Spreadsheet:
             print(f'Opened sheet: {self.sheet_name}')
         except gspread.SpreadsheetNotFound:
             print('Spreadsheet does not exist.')
+
+
+# Populate empty cells with boolean values
+# Useful when building data-dashboard
+def organize_data():
+    sheet = Spreadsheet('Applications', 'Main').sheet  # Open sheet
+    cell_list = sheet.range('I2:I566')
+
+    for cell in range(len(cell_list)):
+        if cell_list[cell].value == 'coverletter':
+            cell_list[cell].value = 'True'
+
+    sheet.update_cells(cell_list)
+
+
+
